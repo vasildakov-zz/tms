348c74e543601b76f7400a211a382f2ac1db2a0a
Vasil Dakov <vasildakov@gmail.com>
2017-01-13 17:45:07 +0000

    Updated docker configs

--- php/run.sh
+++ php/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod -R 777 /var/www/html/data
-
 # Cron Environment Setup
 chmod 644 /etc/crontab
 crontab /etc/crontab
@@ -10,5 +8,12 @@ chmod 777 /var/log/cron.log
 env >> /etc/environment
 service cron start
 
+# Copy xdebug config
+# mv /usr/local/etc/php/config/xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
+
 # Start supervisord and services
 /usr/bin/supervisord -c /etc/supervisord.conf
+
+chmod -R 777 /var/www/html/data
+
+composer install --no-interaction --verbose
