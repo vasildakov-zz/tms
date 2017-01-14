2c0dbd525a92d1c27789aa4150941bb41a69abe8
Vasil Dakov <vasildakov@gmail.com>
2017-01-14 22:52:14 +0000

    Updated console

--- appcode/console.php
+++ appcode/console.php
@@ -13,6 +13,13 @@ $container = require __DIR__ . '/config/container.php';
 $config = $container->get('config');
 
 /** @var \Symfony\Component\Console\Application $cli */
-$cli = new Application('Command Line Interface', 1.0);
+$cli = new Application('TMS Command Line Interface', 1.0);
 
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet([
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper(
+        $container->get('doctrine.entity_manager.orm_default')
+    ),
+]);
+
+$cli->setHelperSet($helperSet);
 $cli->run();
