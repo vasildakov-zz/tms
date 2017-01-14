821e5ffaa4c934901bcb37c58c2b123085e52ea5
Vasil Dakov <vasildakov@gmail.com>
2017-01-14 22:53:50 +0000

    Created customer repository

---
+++ appcode/src/Infrastructure/Doctrine/Repository/CustomerRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Infrastructure\Doctrine\Repository;
+
+use Domain\Repository\CustomerRepositoryInterface;
+
+class CustomerRepository implements CustomerRepositoryInterface
+{
+    //
+}
