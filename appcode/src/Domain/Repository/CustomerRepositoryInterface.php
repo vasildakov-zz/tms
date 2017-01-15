dab57b315f10a94c5cb8b203ca1f49246be28190
Vasil Dakov <vasildakov@gmail.com>
2017-01-15 23:11:12 +0000

    updated dockblocks

--- appcode/src/Domain/Repository/CustomerRepositoryInterface.php
+++ appcode/src/Domain/Repository/CustomerRepositoryInterface.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the vasildakov/tms project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Vasil Dakov <vasildakov@gmail.com>
+ * @link https://github.com/vasildakov/tms GitHub
+ */
 
 namespace Domain\Repository;
 
