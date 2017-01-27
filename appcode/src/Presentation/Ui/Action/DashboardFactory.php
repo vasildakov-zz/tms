136c05462713111413eb3f5bb71a51aec44abb28
Vasil Dakov <vasildakov@gmail.com>
2017-01-27 15:52:59 +0000

    Updated dashboard factory

--- appcode/src/Presentation/Ui/Action/DashboardFactory.php
+++ appcode/src/Presentation/Ui/Action/DashboardFactory.php
@@ -21,6 +21,23 @@ class DashboardFactory
     {
         $config = $container->get('config');
         var_dump($config['databases']);
+
+
+        $em  = $container->get('doctrine.entity_manager.orm_default');
+
+        $tool = $container->get(\Doctrine\ORM\Tools\SchemaTool::class);
+
+        $validator = $container->get(\Doctrine\ORM\Tools\SchemaValidator::class);
+
+        //var_dump($validator->validateMapping());
+
+        $classes = [
+            $em->getClassMetadata(\Domain\Entity\Customer::class),
+            $em->getClassMetadata(\Domain\Entity\Payment::class),
+        ];
+
+        $tool->updateSchema($classes);
+
         exit();
 
 
