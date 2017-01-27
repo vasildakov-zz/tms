a67a43477232f7816d68442b1ece441bcef1a205
Vasil Dakov <vasildakov@gmail.com>
2017-01-27 15:52:38 +0000

    Updated doctrine orm schema and validation tools factories

--- appcode/src/Infrastructure/Doctrine/Tools/SchemaToolFactory.php
+++ appcode/src/Infrastructure/Doctrine/Tools/SchemaToolFactory.php
@@ -1,9 +1,20 @@
-<?php
-$tool = new \Doctrine\ORM\Tools\SchemaTool($em);
+<?php declare(strict_types = 1);
 
-$classes = array(
-    $em->getClassMetadata('Entities\User'),
-    $em->getClassMetadata('Entities\Profile')
-);
+namespace Infrastructure\Doctrine\Tools;
 
-$tool->createSchema($classes);
+use Interop\Container\ContainerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class SchemaToolFactory
+{
+    /**
+     * @param ContainerInterface $container
+     * @return Doctrine\ORM\Tools\SchemaTool
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        $em = $container->get('doctrine.entity_manager.orm_default');
+
+        return new SchemaTool($em);
+    }
+}
--- appcode/src/Infrastructure/Doctrine/Tools/SchemaValidatorFactory.php
+++ appcode/src/Infrastructure/Doctrine/Tools/SchemaValidatorFactory.php
@@ -1,10 +1,20 @@
-<?php
+<?php declare(strict_types = 1);
+
+namespace Infrastructure\Doctrine\Tools;
+
+use Interop\Container\ContainerInterface;
 use Doctrine\ORM\Tools\SchemaValidator;
 
-$validator = new SchemaValidator($entityManager);
-$errors = $validator->validateMapping();
+class SchemaValidatorFactory
+{
+    /**
+     * @param ContainerInterface $container
+     * @return Doctrine\ORM\Tools\SchemaValidator
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        $em = $container->get('doctrine.entity_manager.orm_default');
 
-if (count($errors) > 0) {
-    // Lots of errors!
-    echo implode("\n\n", $errors);
+        return new SchemaValidator($em);
+    }
 }
