a03e1c7b448bddd184a0cdf5049587ecc7203e2a
Vasil Dakov <vasildakov@gmail.com>
2017-01-28 23:46:12 +0000

    Updated application docblocks

--- appcode/src/Application/Schema/CreateSchemaCommand.php
+++ appcode/src/Application/Schema/CreateSchemaCommand.php
@@ -1,4 +1,13 @@
 <?php declare(strict_types = 1);
+/**
+ * This file is part of the vasildakov/tms project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Vasil Dakov <vasildakov@gmail.com>
+ * @link https://github.com/vasildakov/tms GitHub
+ */
 
 namespace Application\Schema;
 
@@ -46,28 +55,32 @@ final class CreateSchemaCommand
     /**
      * @return string
      */
-    public function driver() : string {
+    public function driver() : string
+    {
         return $this->driver;
     }
 
     /**
      * @return string
      */
-    public function user() : string {
+    public function user() : string
+    {
         return $this->user;
     }
 
     /**
      * @return string
      */
-    public function password() : string {
+    public function password() : string
+    {
         return $this->password;
     }
 
     /**
      * @return string
      */
-    public function database() : string {
+    public function database() : string
+    {
         return $this->database;
     }
 }
