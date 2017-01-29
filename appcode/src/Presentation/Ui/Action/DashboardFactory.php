9c9c12375f48716ba320f7cbce30114959d02ee9
Vasil Dakov <vasildakov@gmail.com>
2017-01-29 10:16:59 +0000

    Updated dashboard factory

--- appcode/src/Presentation/Ui/Action/DashboardFactory.php
+++ appcode/src/Presentation/Ui/Action/DashboardFactory.php
@@ -28,28 +28,6 @@ class DashboardFactory
      */
     public function __invoke(ContainerInterface $container)
     {
-        $config = $container->get('config');
-        var_dump($config['databases']);
-
-
-        $em  = $container->get('doctrine.entity_manager.orm_default');
-
-        $tool = $container->get(\Doctrine\ORM\Tools\SchemaTool::class);
-
-        $validator = $container->get(\Doctrine\ORM\Tools\SchemaValidator::class);
-
-        //var_dump($validator->validateMapping());
-
-        $classes = [
-            $em->getClassMetadata(\Domain\Entity\Customer::class),
-            $em->getClassMetadata(\Domain\Entity\Payment::class),
-        ];
-
-        $tool->updateSchema($classes);
-
-        exit();
-
-
         if (!$container->has(CommandBus::class)) {
             throw new \Exception("CommandBus is not configured");
         }
