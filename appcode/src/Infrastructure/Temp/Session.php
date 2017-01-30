e9b1bf43e3eb3085f6f4bd311a066681ae4d891b
Vasil Dakov <vasildakov@gmail.com>
2017-01-30 23:38:27 +0000

    Added links

--- appcode/src/Infrastructure/Temp/Session.php
+++ appcode/src/Infrastructure/Temp/Session.php
@@ -1,6 +1,7 @@
 <?php
 // https://zendframework.github.io/zend-session/manager/
-// 
+// http://stackoverflow.com/questions/8990195/how-to-use-zend-session-in-zf2
+// http://stackoverflow.com/questions/20456500/zf2-get-global-session-container
 
 
 use Zend\Session\Config\StandardConfig;
@@ -12,6 +13,7 @@ $config->setOptions([
     'remember_me_seconds' => 1800,
     'name'                => 'zf2',
 ]);
+
 $manager = new SessionManager($config);
 
 $container = new Container('namespace');
