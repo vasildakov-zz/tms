1a137e9aefb5ff193a12e52370a2a8a3d2275f8b
Vasil Dakov <vasildakov@gmail.com>
2017-01-30 12:24:55 +0000

    Expressive bootstrap refactoring

--- appcode/public/ui.php
+++ appcode/public/ui.php
@@ -1,37 +1,14 @@
 <?php
-// use Zend\Expressive\AppFactory;
-// use Zend\ServiceManager\Config;
-// use Zend\ServiceManager\ServiceManager;
-// use Zend\Expressive\Router\ZendRouter;
 
-// chdir(dirname(__DIR__));
-// require 'vendor/autoload.php';
+use Zend\Stdlib\ArrayObject;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Stdlib\Glob;
 
+use Zend\ServiceManager\Config;
+use Zend\ServiceManager\ServiceManager;
 
-// // Config
-// $config = [
-//     'dependencies' => []
-// ];
-
-// // Router
-// $router = new ZendRouter();
-
-// // Build container
-// $container = new ServiceManager();
-// (new Config($config['dependencies']))->configureServiceManager($container);
-
-// // Build app
-// $app = AppFactory::create($container, $router);
-
-// $app->get('/', function ($request, $response, $next) {
-//     $response->getBody()->write('Hello, world!');
-//     return $response;
-// });
-
-// $app->pipeRoutingMiddleware();
-// $app->pipeDispatchMiddleware();
-// $app->run();
-
+use Zend\Expressive\ConfigManager\ConfigManager;
+use Zend\Expressive\ConfigManager\PhpFileProvider;
 
 // Delegate static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server'
@@ -43,9 +20,26 @@ if (php_sapi_name() === 'cli-server'
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
+/** @var \Zend\Expressive\ConfigManager\ConfigManager $configManager */
+$configManager = new ConfigManager([
+    function () {
+        return [ConfigManager::ENABLE_CACHE => true];
+    },
+    Presentation\Ui\ModuleConfig::class,
+    new PhpFileProvider('config/autoload/{{,*.}global,{,*.}local}.php'),
+]);
+
+/** @var \Zend\Stdlib\ArrayObject $config */
+$config = new ArrayObject($configManager->getMergedConfig());
+
+
 /** @var \Interop\Container\ContainerInterface $container */
-$container = require 'config/container.php';
+$container = new ServiceManager();
+(new Config($config['dependencies']))->configureServiceManager($container);
+
+// Inject config
+$container->setService('config', $config);
 
 /** @var \Zend\Expressive\Application $app */
-$app = $container->get('Zend\Expressive\Application');
+$app = $container->get(Zend\Expressive\Application::class);
 $app->run();
